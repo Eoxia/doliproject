ec0c34e617b258c4a899fdcda08b5edce2099d1d
Nicolas Domenech <nicolas@eoxia.com>
2021-10-13 17:28:46 +0200

    [Mod] fix: edit extrafields fk_task

--- core/modules/modDoliproject.class.php
+++ core/modules/modDoliproject.class.php
@@ -180,7 +180,7 @@ class modDoliproject extends DolibarrModules
 		$extra_fields->addExtraField('fk_facture_name', 'Facture', 'link', 100, NULL, 'projet_task', 1, 0, NULL, $param, 1, 1, 1); //extrafields task
 		unset($param);
 		$extra_fields->delete('fk_task', 'ticket');
-		$extra_fields->addExtraField('fk_task', 'Tâche', 'sellist', 100, NULL, 'ticket', 0, 0, NULL, 'a:1:{s:7:"options";a:1:{s:110:"projet_task:ref:rowid::entity = $ENTITY$ AND fk_projet = ($SEL$ fk_project FROM llx_ticket WHERE rowid = $ID$)";N;}}', 0, 1, 'preg_match(\'/public/\',$_SERVER[\'PHP_SELF\'])?0:1'); //extrafields ticket
+		$extra_fields->addExtraField('fk_task', 'Tâche', 'sellist', 100, NULL, 'ticket', 0, 0, NULL, 'a:1:{s:7:"options";a:1:{s:110:"projet_task:ref:rowid::entity = $ENTITY$ AND fk_projet = ($SEL$ fk_project FROM llx_ticket WHERE rowid = $ID$)";N;}}', 1, 1, 'preg_match(\'/public/\',$_SERVER[\'PHP_SELF\'])?0:1'); //extrafields ticket
 
 		return $this->_init($sql, $options);
 	}
