a914020921f1711e1bf66a96de066b3a4068ff13
Nicolas Domenech <nicolas@eoxia.com>
2022-08-25 16:50:27 +0200

    [JS] add: tooltip action

--- js/doliproject.js.php
+++ js/doliproject.js.php
@@ -120,6 +120,131 @@ if ( ! window.eoxiaJS.scriptsLoaded ) {
 	$( document ).ready( window.eoxiaJS.init );
 }
 
+/**
+ * @namespace EO_Framework_Tooltip
+ *
+ * @author Eoxia <dev@eoxia.com>
+ * @copyright 2015-2018 Eoxia
+ */
+
+if ( ! window.eoxiaJS.tooltip ) {
+
+	/**
+	 * [tooltip description]
+	 *
+	 * @memberof EO_Framework_Tooltip
+	 *
+	 * @type {Object}
+	 */
+	window.eoxiaJS.tooltip = {};
+
+	/**
+	 * [description]
+	 *
+	 * @memberof EO_Framework_Tooltip
+	 *
+	 * @returns {void} [description]
+	 */
+	window.eoxiaJS.tooltip.init = function() {
+		window.eoxiaJS.tooltip.event();
+	};
+
+	window.eoxiaJS.tooltip.tabChanged = function() {
+		$( '.wpeo-tooltip' ).remove();
+	}
+
+	/**
+	 * [description]
+	 *
+	 * @memberof EO_Framework_Tooltip
+	 *
+	 * @returns {void} [description]
+	 */
+	window.eoxiaJS.tooltip.event = function() {
+		$( document ).on( 'mouseenter touchstart', '.wpeo-tooltip-event:not([data-tooltip-persist="true"])', window.eoxiaJS.tooltip.onEnter );
+		$( document ).on( 'mouseleave touchend', '.wpeo-tooltip-event:not([data-tooltip-persist="true"])', window.eoxiaJS.tooltip.onOut );
+	};
+
+	window.eoxiaJS.tooltip.onEnter = function( event ) {
+		window.eoxiaJS.tooltip.display( $( this ) );
+	};
+
+	window.eoxiaJS.tooltip.onOut = function( event ) {
+		window.eoxiaJS.tooltip.remove( $( this ) );
+	};
+
+	/**
+	 * [description]
+	 *
+	 * @memberof EO_Framework_Tooltip
+	 *
+	 * @param  {void} event [description]
+	 * @returns {void}       [description]
+	 */
+	window.eoxiaJS.tooltip.display = function( element ) {
+		var direction = ( $( element ).data( 'direction' ) ) ? $( element ).data( 'direction' ) : 'top';
+		var el = $( '<span class="wpeo-tooltip tooltip-' + direction + '">' + $( element ).attr( 'aria-label' ) + '</span>' );
+		var pos = $( element ).position();
+		var offset = $( element ).offset();
+		$( element )[0].tooltipElement = el;
+		$( 'body' ).append( $( element )[0].tooltipElement );
+
+		if ( $( element ).data( 'color' ) ) {
+			el.addClass( 'tooltip-' + $( element ).data( 'color' ) );
+		}
+
+		var top = 0;
+		var left = 0;
+
+		switch( $( element ).data( 'direction' ) ) {
+			case 'left':
+				top = ( offset.top - ( el.outerHeight() / 2 ) + ( $( element ).outerHeight() / 2 ) ) + 'px';
+				left = ( offset.left - el.outerWidth() - 10 ) + 3 + 'px';
+				break;
+			case 'right':
+				top = ( offset.top - ( el.outerHeight() / 2 ) + ( $( element ).outerHeight() / 2 ) ) + 'px';
+				left = offset.left + $( element ).outerWidth() + 8 + 'px';
+				break;
+			case 'bottom':
+				top = ( offset.top + $( element ).height() + 10 ) + 10 + 'px';
+				left = ( offset.left - ( el.outerWidth() / 2 ) + ( $( element ).outerWidth() / 2 ) ) + 'px';
+				break;
+			case 'top':
+				top = offset.top - el.outerHeight() - 4  + 'px';
+				left = ( offset.left - ( el.outerWidth() / 2 ) + ( $( element ).outerWidth() / 2 ) ) + 'px';
+				break;
+			default:
+				top = offset.top - el.outerHeight() - 4  + 'px';
+				left = ( offset.left - ( el.outerWidth() / 2 ) + ( $( element ).outerWidth() / 2 ) ) + 'px';
+				break;
+		}
+
+		el.css( {
+			'top': top,
+			'left': left,
+			'opacity': 1
+		} );
+
+		$( element ).on("remove", function() {
+			$( $( element )[0].tooltipElement ).remove();
+
+		} );
+	};
+
+	/**
+	 * [description]
+	 *
+	 * @memberof EO_Framework_Tooltip
+	 *
+	 * @param  {void} event [description]
+	 * @returns {void}       [description]
+	 */
+	window.eoxiaJS.tooltip.remove = function( element ) {
+		if ( $( element )[0] && $( element )[0].tooltipElement ) {
+			$( $( element )[0].tooltipElement ).remove();
+		}
+	};
+}
 
 /**
  * Initialise l'objet "task" ainsi que la méthode "init" obligatoire pour la bibliothèque EoxiaJS.
